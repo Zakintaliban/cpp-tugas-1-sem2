286e361cbdf5211019d6fc42a585dd1365fa94b5
Khasbullah Nukman Zakin <nukmans23@gmail.com>
2022-05-03 21:45:39 +0700

    updating

--- nomor4.cpp
+++ nomor4.cpp
@@ -5,11 +5,19 @@ int main ()
     int arr[10], n, i, sum = 0;
     cout << "Enter the size of the array : ";
     cin >> n;
-    cout << "\nEnter the elements of the array : ";
-    for (i = 0; i < n; i++)
+    cout << "\nEnter the elements of the array : \n";
+    for (i = 0; i < n; i++){
+    cout << "- Element number " << i + 1 << ": ";
     cin >> arr[i];
-    for (i = 0; i < n; i++)
-    {
+}
+    cout << endl;
+    cout << "Array: \n";
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    for (i = 0; i < n; i++){
         sum += arr[i];
     }
     cout << "\nSum of array elements : " << sum;
