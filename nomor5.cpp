541d863cbf94be040c21484438d3ae4ae2d6d004
Khasbullah Nukman Zakin <nukmans23@gmail.com>
2022-05-03 22:17:54 +0700

    clean code

--- nomor5.cpp
+++ nomor5.cpp
@@ -1,6 +1,3 @@
-// C++ Program to display all elements
-// of an initialised two dimensional array
-
 #include <iostream>
 using namespace std;
 
@@ -8,14 +5,11 @@ int main() {
     int test[4][3] = {{4,7,2},
                       {5,9,1},
                       {8,3,6},
-                      {3,2,5},                    
+                      {3,2,5},
     };
 
-    // use of nested for loop
-    // access rows of the array
     for (int i = 0; i < 4; ++i) {
         cout << endl;
-        // access columns of the array
         for (int j = 0; j < 3; ++j) {
             cout << test[i][j] << " ";
         }
